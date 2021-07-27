a6294353683cb164848143a6b56bb08bf329ad1f
Loh Wen He <wenheloh@hotmail.com>
2021-07-27 23:05:36 +0800

    cleanup unused files

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -1,9 +1,8 @@
-import { mount } from '@vue/test-utils'
-import NuxtLogo from '@/components/NuxtLogo.vue'
+// import { mount } from "@vue/test-utils";
 
-describe('NuxtLogo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(NuxtLogo)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
+xdescribe("NuxtLogo", () => {
+	test("is a Vue instance", () => {
+		// const wrapper = mount(NuxtLogo);
+		// expect(wrapper.vm).toBeTruthy();
+	});
+});

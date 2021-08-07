a84277038cf75b5f35f00b459cf4140888cfb6ed
Loh Wen He <wenheloh@hotmail.com>
2021-08-07 23:18:10 +0800

    debug ssrapp

--- pages/_chart.vue
+++ pages/_chart.vue
@@ -16,8 +16,10 @@ import axios from "axios";
 import { DataSourceType } from "~/common/custom-typings/enums";
 
 const fetchData = async (type: DataSourceType): Promise<CompositeRawDataType[]> => {
+	const baseURL = process.env.apiBaseUrl;
+	console.log("❤️ baseURL when fetching: ", baseURL)
 	const { data: { data: rawData } }: { data: { data: CompositeRawDataType[] } } = await axios({
-		baseURL: process.env.apiBaseUrl,
+		baseURL,
 		url: `/data/${type}`,
 		method: "GET"
 	})
